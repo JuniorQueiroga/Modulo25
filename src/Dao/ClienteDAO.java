69ccd8cb15866f1bbe018675277ad8a6fab96598
JuniorQueiroga <antonio.augusto.7@gmail.com>
2024-11-19 16:55:07 -0300

    Update ClienteDAO.java

--- src/Dao/ClienteDAO.java
+++ src/Dao/ClienteDAO.java
@@ -1,4 +1,27 @@
 package Dao;
 
-public class ClienteDAO {
+import Cliente.Cliente;
+
+public class ClienteDAO implements IClienteDAO {
+
+    @Override
+    public Boolean salvar(Cliente cliente) {
+        return true;
+    }
+
+    @Override
+    public Cliente buscarPorCPF(Long CPF) {
+        return null;
+
+    }
+
+    @Override
+    public void excluir(Long cpf) {
+
+    }
+
+    @Override
+    public void alterar(Cliente cliente) {
+
+    }
 }
