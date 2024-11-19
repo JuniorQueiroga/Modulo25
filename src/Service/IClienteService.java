740f9fdb27e32474598b9b7b59041e0ef8de51a5
JuniorQueiroga <antonio.augusto.7@gmail.com>
2024-11-19 16:59:45 -0300

    Update IClienteService.java

--- src/Service/IClienteService.java
+++ src/Service/IClienteService.java
@@ -1,4 +1,14 @@
 package Service;
 
-public class IClienteService {
+import Cliente.Cliente;
+
+public interface IClienteService {
+
+    Boolean salvar(Cliente cliente);
+
+    Cliente buscarPorCPF(Long cpf);
+
+    void excluir(Long cpf);
+
+    void alterar(Cliente cliente);
 }
