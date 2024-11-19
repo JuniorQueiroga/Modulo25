bb81ed6f8231fbcdca4047cfe43c2805c00b5278
JuniorQueiroga <antonio.augusto.7@gmail.com>
2024-11-19 16:55:21 -0300

    Update ClienteDaoMock.java

--- src/Dao/ClienteDaoMock.java
+++ src/Dao/ClienteDaoMock.java
@@ -1,4 +1,29 @@
 package Dao;
 
-public class ClienteDaoMock {
+import Cliente.Cliente;
+
+public class ClienteDaoMock implements IClienteDAO {
+    @Override
+    public Boolean salvar(Cliente cliente) {
+        return true;
+
+    }
+
+    @Override
+    public Cliente buscarPorCPF(Long cpf) {
+        Cliente cliente = new Cliente();
+        cliente.setCpf(cpf);
+        return cliente;
+    }
+
+    @Override
+    public void excluir(Long cpf) {
+
+    }
+
+    @Override
+    public void alterar(Cliente cliente) {
+
+    }
+
 }
